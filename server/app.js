2b906d2a27dd3d1b08e97391ba3c623640b70c30
Eric B <eric_baffour-addo@alumni.brown.edu>
2021-09-21 07:04:05 -0700

    created new express server

---
+++ server/app.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const path = require("path");
+
+//start our express application
+const app = express();
+
+const port = process.env.PORT || 5000;
+
+//create paths
+const publicFilesPath = path.join(__dirname, "build");
+
+//serve up this static file(which connects to our react buld folder)
+app.use(express.static(path.join(publicFilesPath)));
+
+app.listen(port, () => {
+  console.log(`Our app is live on port ${port}`);
+});
