474d5e61417c102044242a2cad8895d531efd56f
Eric B <eric_baffour-addo@alumni.brown.edu>
2021-09-22 02:33:46 -0700

    ran build

--- server/app.js
+++ server/app.js
@@ -1,15 +1,12 @@
 const express = require("express");
 const path = require("path");
 
-//start our express application
 const app = express();
 
 const port = process.env.PORT || 5000;
 
-//create paths
 const publicFilesPath = path.join(__dirname, "build");
 
-//serve up this static file(which connects to our react buld folder)
 app.use(express.static(path.join(publicFilesPath)));
 
 app.get("*", function (req, res) {
